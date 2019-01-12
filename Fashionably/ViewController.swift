4f28ad03dc92e9f48bdfd072fbf7fa2812ba4894
Hrithvik Alex <halex623@v1020-wn-86-71.campus-dynamic.uwaterloo.ca>
2019-01-12 15:10:58 -0500

    Initial Commit

---
+++ Fashionably/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fashionably
+//
+//  Created by Hrithvik  Alex on 2019-01-12.
+//  Copyright © 2019 Hrithvik  Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
